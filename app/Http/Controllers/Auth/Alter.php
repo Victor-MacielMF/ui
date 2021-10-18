f441130b9675fb59382e0287f5f96fbbe955ad52
Victor-MacielMF <joaovictormacieldefreitas2345@gmail.com>
2021-10-18 14:31:07 -0300

    Editar endereço

--- app/Http/Controllers/Auth/Alter.php
+++ app/Http/Controllers/Auth/Alter.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Endereco;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Alter extends Controller
@@ -10,6 +12,21 @@ class Alter extends Controller
 
     public function index()
     {
-        return view('usuario.view');
+        $user = auth()->user();
+        $enderecos = $user->enderecos;
+        return view('usuario.view', ['user'=>$user, 'enderecos' => $enderecos]);
+    }
+
+    public function destroy($id){
+        Endereco::findOrFail($id)->delete();
+        return redirect('/meus-dados')->with('msg','Endereço excluído com sucesso!');
+    }
+
+    public function updateAddress(Request $request){
+        $data = $request->all();
+        
+        Endereco::findOrFail($request->id)->update($data);
+        return redirect('/meus-dados')->with('msg', 'Evento editado com sucesso!');
+
     }
 }
